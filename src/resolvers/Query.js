9cfbad3aae9f6817ec006ead9bbd82fbacf73bc8
Jeseekia <jeseekia@gmail.com>
2021-04-23 00:19:34 -0400

    Enable asc,desc sorting on feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -7,7 +7,8 @@ async function feed(parent, args, context, info) {
   const links = await context.prisma.link.findMany({
     where,
     skip: args.skip,
-    take: args.take
+    take: args.take,
+    orderBy: args.orderBy
   })
   return links
 }
